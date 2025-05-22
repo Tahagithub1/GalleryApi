55e9cd8ef215c0b7f0ef9a266aedb1cda0e101ee
Paradox <mogevaraluffyy@gmail.com>
2025-05-22 18:26:59 +0330

    MakeSeedAndFactory

---
+++ database/seeders/DummyDataSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DummyDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::inRandomOrder()->take(5)->get();
+
+        foreach ($users as $user) {
+            Message::factory()->count(3)->create([
+                'user_id' => $user->id,
+            ]);
+        }
+    }
+}
