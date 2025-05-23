7e0ad9ce07ac98c004c65072160b2ae3f3310a75
Paradox <mogevaraluffyy@gmail.com>
2025-05-23 23:01:49 +0330

    images

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\API\MessageController;
+use Illuminate\Support\Facades\File;
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -13,3 +15,16 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 Route::middleware(['auth:api'])->post('/messages', [MessageController::class, 'store']);
+
+Route::get('/images', function () {
+    $files = File::files(storage_path('app/public/images'));
+
+    $images = collect($files)->map(function ($file) {
+        return [
+            'name' => $file->getFilename(),
+            'url' => asset('storage/images/' . $file->getFilename()),
+        ];
+    });
+
+    return response()->json($images);
+});
